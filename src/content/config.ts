b53c35c752b2f1d91b01d5920618979e5e336292
luoqiucc <luoqiucc@outlook.com>
2025-01-18 21:21:30 +0800

    布局调整

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,18 @@
 import { defineCollection } from 'astro:content'
 import { postSchema } from '../schemas/postSchema.ts'
+import { momentSchema } from '../schemas/momentSchema.ts'
 
 const postCollection = defineCollection({
   type: 'content',
   schema: postSchema,
 })
 
+const momentCollection = defineCollection({
+  type: 'content',
+  schema: momentSchema,
+})
+
 export const collections = {
   posts: postCollection,
+  moments: momentCollection,
 }
