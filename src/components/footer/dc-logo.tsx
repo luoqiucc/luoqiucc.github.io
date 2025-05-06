ebb04cb5558c56fc58bf8b18570ebbffa34985d7
luoqiucc <luoqiucc@outlook.com>
2025-05-06 10:54:21 +0800

    update

---
+++ src/components/footer/dc-logo.tsx
@@ -0,0 +1,10 @@
+export default function DCLogo() {
+  return (
+    <div>
+      <h2 className="scroll-m-20 text-3xl italic font-semibold tracking-tight first:mt-0">
+        动核互联
+      </h2>
+      <small className="text-sm leading-none font-semibold">DYNAMIC CORE</small>
+    </div>
+  )
+}
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,11 @@
+import DCLogo from './dc-logo'
+
 export default function Footer() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 bg-muted/50">
-      <div className="mx-auto h-32 w-full max-w-5xl"></div>
+      <div className="mx-auto h-32 w-full max-w-5xl py-4">
+        <DCLogo />
+      </div>
     </div>
   )
 }
