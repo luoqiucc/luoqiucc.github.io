008535c626900c31c503895487c3a0a05a70f25c
luoqiucc <luoqiucc@outlook.com>
2025-05-11 20:07:50 +0800

    引入utteranc

---
+++ src/components/comment/Utterances.tsx
@@ -0,0 +1,15 @@
+export default function Utterances() {
+  return (
+    <div>
+      <script
+        src="https://utteranc.es/client.js"
+        repo="luoqiucc/luoqiucc.github.io"
+        issue-term="pathname"
+        theme="github-dark"
+        label="comment"
+        crossorigin="anonymous"
+        async
+      ></script>
+    </div>
+  )
+}
