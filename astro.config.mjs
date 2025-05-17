95d2721706588fa9ecfcf0a539e42e29ab23185c
luoqiucc <luoqiucc@outlook.com>
2025-05-17 23:52:30 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-
 import react from '@astrojs/react'
-
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luoqiucc.github.io/',
-
   server: {
     port: 4321,
     host: true,
   },
-
-  image: {
-    domains: ['https://img.picui.cn'],
-  },
-
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react(), mdx()],
 })
