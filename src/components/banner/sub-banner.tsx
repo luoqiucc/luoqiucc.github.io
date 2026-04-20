232f33b5e013535b2dbd2875cd65f3bfea0a96a9
luoqiucc <luoqiucc@outlook.com>
2026-04-20 11:10:58 +0800

    update

--- src/components/banner/sub-banner.tsx
+++ src/components/banner/sub-banner.tsx
@@ -3,7 +3,7 @@ function SubBanner() {
     <div className="bg-blue-500">
       <a href="/">
         <div className="mx-auto w-full max-w-5xl p-1 text-center text-sm leading-8 text-white">
-          Hi, 这里是我的小站，期待可以有所帮助...
+          Hi，这里是我的小站，期待可以有所帮助...
         </div>
       </a>
     </div>
