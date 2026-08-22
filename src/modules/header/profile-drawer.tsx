c8d689a26c9e6ccc506a2504a54c0b11d55d4757
luoqiucc <luoqiucc@outlook.com>
2026-08-22 18:54:13 +0800

    更新论文信息

--- src/modules/header/profile-drawer.tsx
+++ src/modules/header/profile-drawer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { X } from "lucide-react"
+import { X, ScrollText } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
   Drawer,
@@ -11,7 +11,14 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer"
-import { Card, CardContent, CardFooter } from "@/components/ui/card"
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Social } from "./social"
 
@@ -27,37 +34,97 @@ function ProfileDrawer() {
         </Avatar>
       </DrawerTrigger>
       <DrawerContent>
-        <DrawerHeader>
-          <div className="flex justify-center">
-            <Avatar className="h-30 w-30">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/109642163?v=4" />
-              <AvatarFallback>L</AvatarFallback>
-            </Avatar>
-          </div>
-          <DrawerTitle className="text-center">落秋cc</DrawerTitle>
-          <DrawerDescription className="text-center">
-            👋🏻 Hi！这里是落秋cc，计算机网络方向，专注于高性能网络技术。熟悉 P4
-            可编程数据平面与 DPDK
-            高速数据包处理，当前主要聚焦于网络传输协议及性能优化相关工作。
-          </DrawerDescription>
-        </DrawerHeader>
-        <div className="my-4">
+        {/* social */}
+        <div className="mb-4">
           <Social />
         </div>
-        <div className="overflow-y-auto p-4">
-          <div>
-            <Card>
-              <CardContent>
-                <p>
-                  我小的时候，父母偶尔会给我拿点零花钱，我拿着这些钱，叫上我的好朋友去上5块钱3个小时的网，就那种县城里的小网吧，昏暗的灯光，包浆的设备，烟雾缭绕的大厅。我和我的好朋友两个人，玩的像过年一样，我至今不能忘怀。后来我上了班，平常也会去上网，网咖也好电竞酒店也好，几十几百一个晚上的私人包间，环境优雅，设备高端，但是我总也找不到小时候5块钱3个小时小网吧的，那样的激动和感觉。
-                </p>
-              </CardContent>
-              <CardFooter>
-                <p className="italic">《我的上网》 @落秋cc</p>
-              </CardFooter>
-            </Card>
+
+        {/* content */}
+        <div className="overflow-y-auto">
+          {/* avatar */}
+          <div className="my-4">
+            <div className="flex justify-center">
+              <Avatar className="h-30 w-30">
+                <AvatarImage src="https://avatars.githubusercontent.com/u/109642163?v=4" />
+                <AvatarFallback>L</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="p-4">
+              <h3 className="scroll-m-20 text-center text-2xl font-semibold tracking-tight">
+                落秋cc
+              </h3>
+              <p className="py-2">
+                👋🏻
+                Hi！这里是落秋cc，硕士在读，计算机网络方向，专注于高性能网络技术。熟悉
+                P4 可编程数据平面，DPDK
+                高速数据包处理，NS3网络仿真工具，当前主要聚焦于网络传输协议及性能优化相关工作。
+              </p>
+            </div>
+          </div>
+
+          {/* work */}
+          <div className="my-4 px-4">
+            <h3 className="mb-4 font-semibold tracking-tight">我参与的工作</h3>
+            <a
+              href="https://dl.acm.org/doi/10.1007/978-981-95-8408-6_28"
+              target="_blank"
+            >
+              <Item variant="muted" className="my-4">
+                <ItemMedia variant="icon">
+                  <ScrollText />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>
+                    Mitigating Hash Polarization with Flow-Level Load Balancing
+                    in Leaf-Spine Data Center Network.
+                  </ItemTitle>
+                  <ItemDescription>
+                    Algorithms and Architectures for Parallel Processing: 25th
+                    International Conference, ICA3PP 2025
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+            </a>
+
+            <a
+              href="https://link.springer.com/chapter/10.1007/978-981-95-8420-8_40"
+              target="_blank"
+            >
+              <Item variant="muted" className="my-4">
+                <ItemMedia variant="icon">
+                  <ScrollText />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>
+                    SMAR: Short-Flow Multi-path Adaptive Routing for
+                    Heterogeneous RDMA Workloads
+                  </ItemTitle>
+                  <ItemDescription>
+                    Algorithms and Architectures for Parallel Processing: 25th
+                    International Conference, ICA3PP 2025
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+            </a>
+
+            <Item variant="muted" className="my-4">
+              <ItemMedia variant="icon">
+                <ScrollText />
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle>
+                  Switch-Transparent Load Balancing for RDMA Data Centers: A
+                  Host-Only Approach
+                </ItemTitle>
+                <ItemDescription>
+                  IEEE/ACM International Symposium on Quality of Service (IWQoS)
+                  2026
+                </ItemDescription>
+              </ItemContent>
+            </Item>
           </div>
         </div>
+
         <DrawerFooter>
           <DrawerClose render={<Button variant="ghost" />}>
             <X />
